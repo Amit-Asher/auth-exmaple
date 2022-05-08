076c9cd4459e5678d65bfc4e1166144c0c3ae6d9
Amit Asher <asheramit4@gmail.com>
2022-05-08 22:02:35 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const app = express();
+const port = 3000;
+
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/generate', (req, res) => {
+    try {
+        const token = jwt.sign(
+            { email: req.body.email, expiresIn: new Date((new Date().getTime() + 5 * 60000)) }, // payload contains identifier + uniqe field (e.g expiration time)
+            process.env.JWT_PK, // secret key from .env
+            { expiresIn: '300s' } // 5 minutes
+        );
+        res.send(token);
+    } catch(err) {
+        res.status(500).send({ 'error': 'please provide email in request body' });
+    }
+});
+
+app.get('/validate', (req, res) => {
+    try {
+        const { token } = req.headers;
+        jwt.verify(token, process.env.JWT_PK, (err, decoded) => {
+            if (err) {
+                res.send('unauthorized');
+            } else {
+                res.send('authorized');
+            }
+        });
+    } catch(err) {
+        res.status(500).send({ 'error': 'unable to process the request' });
+    }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
