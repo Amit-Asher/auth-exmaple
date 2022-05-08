50b824e08b842d475ebe073d6d168b8c9083d2c5
Amit Asher <asheramit4@gmail.com>
2022-05-08 22:15:30 +0300

    python tests

---
+++ tests/validate-token.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+url = "http://localhost:3000/validate"
+
+payload = json.dumps({
+  "email": "amit"
+})
+headers = {
+  'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFtaXQiLCJleHBpcmVzSW4iOiIyMDIyLTA1LTA4VDE5OjEwOjA5LjY4OVoiLCJpYXQiOjE2NTIwMzY3MDksImV4cCI6MTY1MjAzNzAwOX0.nYUmr02s0686SINKXAGOF8OEfefVgKI8HyFS-8S9lEM',
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
